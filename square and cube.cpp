ecd675cdf1a5687a1580aa94af766e82b8f13d5b
vidit013 <127816380+vidit013@users.noreply.github.com>
2023-04-07 09:08:11 +0530

    Add files via upload
    Module 2

---
+++ square and cube.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a, square, cube;
+	printf("enter the value of a :- ");
+	scanf("%d", &a);
+	square=a*a;
+	cube=a*a*a;
+	printf("square is %d \n",square);
+	printf("cube is %d",cube);
+	
+}
